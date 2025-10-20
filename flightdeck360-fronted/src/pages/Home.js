a2849651c6583f22e3f64deba30bb606a245bc37
ramya-1993 <ramyas09111@gmail.com>
2025-10-20 18:15:19 +0530

    Delete flightdeck360-fronted directory

--- flightdeck360-fronted/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function Home(){
-  return (
-    <div>
-      <h2>Welcome to FlightDeck360</h2>
-      <p>Book flights easily. Register as a Passenger or Admin to manage flights.</p>
-    </div>
-  );
-}
